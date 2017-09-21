8aff39da458b5b1ae16e834dbc2b4b090bcbbed2
michaeltreat <michael.leon.treat@gmail.com>
2017-09-21 08:53:46 -0700

    Fixed broken linter, added a git ignore, and started the structuring of the models

---
+++ public/model/anonUserModel.js
@@ -0,0 +1,11 @@
+'use strict';
+var x = 1;
+var x = 2;
+
+const anonUser = {
+  selectedUser: ""
+};
+user.getSteamData = function(selectedUser, callback){
+  // Makeasf call to get the correct Steam Id in the event that a
+  // user enters in url-number instead of a vanity url.
+};
