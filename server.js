2e709ce414cd342b6afc8011b173ca65c678238e
michaeltreat <michael.leon.treat@gmail.com>
2017-09-21 18:12:20 -0700

    Finished building out the psql database in the server. Running 'node server' in the command line will start the server, and then the server will connect to the pg database either using the build's 'process.env.DATABASE' or the hardset local database. Also created a function that will create a leaderboard table if non exists. Next I will start writing out the expected routes.

--- server.js
+++ server.js
@@ -3,39 +3,62 @@
 const pg = require('pg') // postgres database
 const request = require('superagent') // AJAX api
 const express = require('express') // middleware connector
-const bodyParser = require('body-parser')
-const PORT = process.env.PORT || 3000
 const app = express()
+const PORT = process.env.PORT || 3000
 
-const conString = process.env.DATABASE || 'postgres://'
+const conString = process.env.DATABASE || 'postgres://@localhost:5432/dyes'
 const client = new pg.Client(conString)
 
-client.connect()
-client.on('error', function(err){
-  console.error(err);
+
+client.connect() // Connects to postgres database
+
+// PSQL database Event Listeners
+client.on('connect', function(){
+  console.log('Connected to the psql database successfully!')
 })
+
+client.on('error', function(err){
+  console.error('Something went wrong involving the database' , err);
+})
+
+// If there's no Leaderboard Table then make one.
+function createLeaderboardTable(){
+  client.query(`
+    CREATE TABLE IF NOT EXISTS leaderboard (
+      leaderboard_id SERIAL PRIMARY KEY,
+      name VARCHAR(255),
+      time VARCHAR(255)
+    )`)
+    .catch(function(err){
+      console.log('Something went wrong with checking/creating the leaderboard tables.',err);
+    });
+}
+createLeaderboardTable()
 
 app.use(express.static('./public'))
 
-app.get('', (req,res) => {
-  request.get()
-  .then()
-  .end(function(err,res){
-    res.send()
-  })
+app.listen( () =>{
+  console.log('connected on port', PORT)
 })
 
-app.post('', (req, res) => {
-
-  request.post().then( () => {
-
-  }).catch()
+app.get('/getSteamData', (req,res) => {
+  // Route to get a user's SteamData.
 })
 
-app.put('', (req, res) => {
-  request.put().then( () => {
+app.get('/checkLeaderboard', (req, res) => {
+  // Route to check if someone is already on the leaderboard.
+})
 
-  }).catch()
+app.post('/addToLeaderboard', (req, res) => {
+  // Route to add someone to the leaderboard for the first time.
+})
+
+app.put('/updateLeaderboard', (req, res) => {
+  // Route to update a score for a user currently on the leaderboard.
+})
+
+app.delete('/removeFromLeaderboard', (req, res) => {
+  // Removes a user from the leaderboard.
 })
 
 app.delete('', (req, res) => {
