195cbcef811a587e3cff11c520786fd1c79798ac
michaeltreat <michael.leon.treat@gmail.com>
2017-09-25 17:48:26 -0700

    Added route to get the user's game data. Also removed the testing req.headers. The routes will now give valid responses when used with proper headers.

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ client.on('connect', function(){
 })
 
 client.on('error', function(err){
-  console.error('Something went wrong involving the database' , err);
+  console.error('Something went wrong involving the database!' , err);
 })
 
 // If there's no Leaderboard Table then make one.
@@ -36,7 +36,6 @@ function createLeaderboardTable(){
 createLeaderboardTable()
 
 // Routing for server
-
 app.use(express.static('./public'))
 app.listen(PORT,function(){
   console.log('Server started on: ', PORT);
@@ -44,25 +43,47 @@ app.listen(PORT,function(){
 })
 
 app.get('/getSteamID', (req,res) => {
-  req.headers.vanityUrl = 'killertreat'
+
   if(req.headers.vanityUrl){
-    request.get(`https://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=${process.env.KEYs}&vanityurl=${req.headers.vanityUrl}`)
-    .end( function(err, response){
-      if(err) {
-        console.log('There was an error while trying to get the SteamID.\n\nerr.message:\n    ', err.status, err.message)
-        res.send(err.status, err.message)
-      }else{
-        console.log(`This is the response.body: ${response.body}`)
-        res.steamid = response.body.response.steamid
-        res.send(res.status, res.steamid)
-      }
-    })
+    request.get(`https://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=${process.env.KEY}&vanityurl=${req.headers.vanityUrl}`)
+      .end( function(err, response){
+        if(err) {
+          console.log('There was an error while trying to get the SteamID.\n\nerr.message:\n    ', err.status, err.message)
+          res.status(`${response.status}`).send(err.status, err.message)
+        }else{
+          console.log(`This is the response.body: ${response.body.response.steamid}`)
+          res.steamid = response.body.response.steamid
+          res.status(`${response.status}`).send(res.steamid)
+        }
+      })
   }else{
     res.status('400').send('400 Bad Request - The vanityUrl was not attached to the headers on your request.')
     console.log('400 bad request- There is noting assigned to req.headers.vanityUrl.')
   }
 })
+
 // Route to get a user's SteamData.
+app.get('/getSteamData', (req,res) => {
+  if(req.headers.steamID){
+
+    request.get(`https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${process.env.KEY}&steamid=${req.headers.steamID}&format=json&include_appinfo=1`)
+
+    .end( function(err, response){
+      if(err){
+        console.log('There was an error trying to get the user\'s game data.\n\n err:\n    ', err.status, err.message)
+        res.status(`${response.status}`).send(err.status, err.message)
+      }else{
+        console.log('This is the response for /getSteamData:\n\nresponse.body:\n    ', response.body.response)
+        res.steamData = response.body
+        res.status(`${response.status}`).send(response.status, res.steamData)
+      }
+    })
+
+  }else{
+    res.status('400').send('400 Bad Request - The steamID was not attached to the headers on the request.')
+    console.log('400 Bad Request - There is nothing assigned to req.headers.steamID');
+  }
+})
 
 app.get('/checkLeaderboard', (req, res) => {
   // Route to check if someone is already on the leaderboard.
@@ -80,10 +101,5 @@ app.delete('/removeFromLeaderboard', (req, res) => {
   // Removes a user from the leaderboard.
 })
 
-app.delete('/delete', (req, res) => {
-  request.delete().then( () => {
 
-  }).catch()
-})
-
-app.get('/*', (request, response) => response.sendFile('./public/index.html', {root: '.'}));
+app.get('/*', (req, res) => res.sendFile('./public/index.html', {root: '.'}));
